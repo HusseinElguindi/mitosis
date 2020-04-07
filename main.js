3eff1b1fd797710a633d990e7a2d15f8aad24405
SHRKV2 <34726859+SHRKV2@users.noreply.github.com>
2020-04-06 21:09:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup()
+{
+    frameRate(60);
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw()
+{
+    background(25);
+
+    credits();
+}
+
+function credits()
+{
+    noStroke();
+    fill(200);
+    textSize(15);
+    textAlign(LEFT, BOTTOM);
+    text("Mitosis\nHussein Elguindi", 10, -30, width, height);
+}

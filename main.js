ef39834db38c59327c12e9836e071706cdc6a4bc
SHRKV2 <34726859+SHRKV2@users.noreply.github.com>
2020-04-07 14:15:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ function setup()
     createCanvas(windowWidth, windowHeight);
     c = new Cell();
 
-    area = width * height;
-    num = int(area * 0.00002);
-
     population = [];
-    for (let i = 0; i < num; i++)
+    for (let i = 0; i < 25; i++)
     {
         population[i] = new Cell();
     }
@@ -26,19 +23,19 @@ function draw()
     this.c.update();
     this.c.show();
 
-    // this.population.forEach(cell => {
-    //     cell.rand_move();
-    //     cell.update();
-    //     cell.show();
-    // });
-
-    for (let i = 0; i < this.population.length; i++)
-    {
-        let cell = this.population[i];
+    this.population.forEach(cell => {
         cell.rand_move();
         cell.update();
         cell.show();
-    }
+    });
+
+    // for (let i = 0; i < this.population.length; i++)
+    // {
+    //     let cell = this.population[i];
+    //     cell.rand_move();
+    //     cell.update();
+    //     cell.show();
+    // }
 }
 
 function credits()

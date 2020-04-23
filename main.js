17f2cb2233c20332cd58cf0ae6722630d74c6f72
SHRKV2 <34726859+SHRKV2@users.noreply.github.com>
2020-04-22 20:04:00 -0400

    v4

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+var resets = 0;
+var settingsWin;
+
 function setup()
 {
     frameRate(60);
     createCanvas(windowWidth, windowHeight);
-    c = new Cell();
+   
+    settingsWin = false;
 
+    c = new Cell();
     population = new Population(25);
+
+    if (resets == 0) 
+    {
+        drawResetBtn();
+        drawSettingBtn();
+    } 
 }
 
 
@@ -13,10 +24,71 @@ function draw()
     background(25);
 
     population.update_all();
-
+    
     stats();
+
+    if (settingsWin)
+    {
+        settings();
+    }
+
     credits();
     // borders();
+    // buttons();
+
+    // drawSettingBtn();
+}
+
+function drawResetBtn()
+{
+    this.resetBtn = createButton("Reset");
+    this.resetBtn.addClass("classicBtn");
+    this.resetBtn.size(70, 30);
+    this.resetBtn.position(width-10-70, 10);
+    this.resetBtn.mousePressed(resetAll);
+}
+
+function drawSettingBtn()
+{
+    this.settingsBtn = createButton("Settings");
+    this.settingsBtn.addClass("classicBtn");
+    this.settingsBtn.size(70, 30);
+    this.settingsBtn.position(width-10-70, 45);
+    this.settingsBtn.mousePressed(function () {
+        if (settingsWin) settingsWin = false;
+        else settingsWin = true;
+    });
+}
+
+function resetAll()
+{
+    resets++;
+    setup();
+}
+
+function settings()
+{
+    rectMode(CENTER);
+    let w = width * 0.8
+    let h = height * 0.5
+
+    fill(255);
+    rect(width/2, height/2, w, h);
+
+    rectMode(CORNER);
+}
+
+function buttons()
+{
+    stroke(150);
+    strokeWeight(1);
+    fill(255);
+    let bw = 60;
+    let bh = 25;
+    rect(10, 70, bw, bh, 15);
+
+    text("Click Delete", x, y, x2, y2)
+    
 }
 
 function credits()
@@ -47,6 +119,10 @@ function borders()
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+
+    this.resetBtn.position(width-10-70, 10);
+    this.settingsBtn.position(width-10-70, 45);
+
     this.population.members.forEach(cell => {
         cell.pos = createVector(random(c.border, width-c.border), random(c.border, height-c.border));
     });
